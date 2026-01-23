8660cbdd90ee36b70f666263dd8dbaf108fa97a9
Govin8 <govin5076@gmail.com>
2026-01-23 16:57:08 +0800

    Level-1: Add echo and bye command

--- src/main/java/Tommy.java
+++ src/main/java/Tommy.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Tommy {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Tommy");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+        }
+
+        scanner.close();
     }
 }

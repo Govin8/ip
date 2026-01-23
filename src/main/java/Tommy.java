ed4fbf71cf9ba1ba4410d5e3861d743d9b3ddb5f
Govin8 <govin5076@gmail.com>
2026-01-23 17:00:22 +0800

    Level-2: Add task storage and list command

--- src/main/java/Tommy.java
+++ src/main/java/Tommy.java
@@ -4,6 +4,9 @@ public class Tommy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Tommy");
         System.out.println("What can I do for you?");
@@ -12,14 +15,24 @@ public class Tommy {
         while (true) {
             String input = scanner.nextLine();
 
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________");
-
             if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + input);
+                System.out.println("____________________________________________________________");
             }
         }
 
